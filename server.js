fc5b9e2d42f6ffab4eaad67a8030258709656d74
Wentyrone12 <jimenezwendell409@gmail.com>
2026-05-23 15:34:21 +0800

    fix

--- server.js
+++ server.js
@@ -64,7 +64,8 @@ app.post("/download", async (req, res) => {
     }
 });
 
-// 🔥 IMPORTANT: START SERVER
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on " + PORT);
 });
